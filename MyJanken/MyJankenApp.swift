f123023d7c9ffcb4d07e3592ec1ddd52d220264f
tanaka <sisyamo2236@gmail.com>
2021-04-19 18:40:20 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by tanaka  on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
